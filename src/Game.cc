a14f77f75ea6f07e77ac786f817daa11b75a145e
MONFOUGA MARIE <marie.monfouga@gmail.com>
2020-03-17 16:39:06 +0100

    #24 affichage d'un cara ascii à partir d'un tableau de caractères

--- src/Game.cc
+++ src/Game.cc
@@ -1,16 +1,19 @@
 #include "Game.hh"
 
-Game::Game(SDL_Window* w)
-    : window_(w) {}
+#include "alphabet.hh"
 
-int Game::getLives() const{return lives_;}
+Game::Game(SDL_Window* w) : window_(w) {
+}
 
-void Game::lostLife(){
+int Game::getLives() const {
+    return lives_;
+}
+
+void Game::lostLife() {
     lives_--;
 }
 
-void draw(int width, int height, SDL_Window* window)
-     {
+void draw(int width, int height, SDL_Window* window) {
     SDL_Renderer* renderer = SDL_GetRenderer(window);
     float mx[3]            = {0.0f, -2.5f, +2.5f};
     float my[3]            = {-5.5f, +2.5f, +2.5f};
@@ -40,18 +43,17 @@ void draw(int width, int height, SDL_Window* window)
     }
 }
 
-void Game::drawLives() const{
+void Game::drawLives() const {
     int width;
     int height;
     SDL_GetWindowSize(window_, &width, &height);
-    
-    for (int i =1; i <= lives_;i++){
-        draw(20*i,20,window_);
-    }
 
+    for (int i = 1; i <= lives_; i++) {
+        draw(20 * i, 20, window_);
+    }
 }
 
-void drawZero(int x, int y, SDL_Window* window){
+void drawZero(int x, int y, SDL_Window* window) {
     SDL_Renderer* renderer = SDL_GetRenderer(window);
     float mx[4]            = {-2.5f, -2.5f, +2.5f, +2.5f};
     float my[4]            = {-5.5f, +5.5f, +5.5f, -5.5f};
@@ -81,9 +83,56 @@ void drawZero(int x, int y, SDL_Window* window){
     }
 }
 
-void Game::drawScore() const{
+void drawexclam(SDL_Window* window, int x, int y, int ascii) {
+    int taille = simplex[ascii][0] * 2;
+    float test[taille];
+
+    int width;
+    int height;
+    SDL_GetWindowSize(window, &width, &height);
+    SDL_Renderer* renderer = SDL_GetRenderer(window);
+
+    int max_X = simplex[ascii][2];
+    int min_X = simplex[ascii][2];
+
+    int max_Y = simplex[ascii][3];
+    int min_Y = simplex[ascii][3];
+
+    for (int i = 2; i < taille + 2; i += 2) {
+        if (simplex[ascii][i] < min_X && simplex[ascii][i] >= 0)
+            min_X = simplex[ascii][i];
+        if (simplex[ascii][i] > max_X)
+            max_X = simplex[ascii][i];
+        if (simplex[ascii][i + 1] < min_Y && simplex[ascii][i + 1] >= 0)
+            min_Y = simplex[ascii][i + 1];
+        if (simplex[ascii][i + 1] > max_Y)
+            max_Y = simplex[ascii][i + 1];
+    }
+
+    for (int i = 0; i < taille; i += 2) {
+        if (simplex[ascii][i + 2] == -1 && simplex[ascii][i + 3] == -1) {
+            test[i]     = -1;
+            test[i + 1] = -1;
+        } else {
+            test[i]     = simplex[ascii][i + 2];
+            test[i + 1] = max_Y - (simplex[ascii][i + 3] - min_Y);
+        }
+    }
+
+    for (int i = 0; i < taille - 2; i += 2) {
+        if (test[i + 2] != -1 && test[i + 3] != -1 && test[i] != -1 &&
+            test[i + 1] != -1)
+            SDL_RenderDrawLineF(renderer,
+                test[i] + x,
+                test[i + 1] + y,
+                test[i + 2] + x,
+                test[i + 3] + y);
+    }
+}
+
+void Game::drawScore() const {
     int width;
     int height;
     SDL_GetWindowSize(window_, &width, &height);
-    drawZero(width-20,20,window_);
+    drawexclam(window_, width - 20, 20, 54 - 32);
 }
